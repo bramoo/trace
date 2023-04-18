da4852e4b06fe7d713f052c79975fcfec8edb449
bramoo <brad.moorfield@gmail.com>
2023-04-18 20:04:46 +1200

    Add defocus blur and final scene of RTIOW

--- src/trace.cpp
+++ src/trace.cpp
@@ -65,6 +65,47 @@ void init_world(hittable_list& world, int scene_id) {
 		world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
 	    }
 	    break;
+	case 3:
+	    {
+		auto ground_mat = make_shared<lambertian>(colour(0.5, 0.5, 0.5));
+		world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_mat));
+		
+		for (int a = -11; a < 11; a++) {
+		    for (int b = -11; b < 11; b++) {
+			auto choose_mat = random_double();
+			point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+			    shared_ptr<material> sphere_mat;
+
+			    if (choose_mat < 0.8) {
+				// diffuse
+				auto albedo = colour::random() * colour::random();
+				sphere_mat = make_shared<lambertian>(albedo);
+				world.add(make_shared<sphere>(center, 0.2, sphere_mat));
+			    } else if (choose_mat < 0.95) {
+				// metal
+				auto albedo = colour::random(0.5, 1);
+				auto fuzz = random_double(0, 0.5);
+				sphere_mat = make_shared<metal>(albedo, fuzz);
+				world.add(make_shared<sphere>(center, 0.2, sphere_mat));
+			    } else {
+				// glass
+				sphere_mat = make_shared<dielectric>(1.5);
+				world.add(make_shared<sphere>(center, 0.2, sphere_mat));
+			    }
+			}
+		    }
+		}
+
+		auto material1 = make_shared<dielectric>(1.5);
+		world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+		auto material2 = make_shared<lambertian>(colour(0.4, 0.2, 0.1));
+		world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+		auto material3 = make_shared<metal>(colour(0.7, 0.6, 0.5), 0.0);
+		world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+	    }
+	    break;
     }
 }
 
@@ -91,7 +132,8 @@ int main(int argc, char *argv[]) {
     }
 
     // image
-    const auto aspect_ratio = 16.0 / 9.0;
+    //const auto aspect_ratio = 16.0 / 9.0;
+    const auto aspect_ratio = 3.0 / 2.0;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     
     const int rays_per_line = image_width * samples_per_pixel;
@@ -104,7 +146,13 @@ int main(int argc, char *argv[]) {
     init_world(world, scene_id);
 
     // camera
-    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20, aspect_ratio);
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
